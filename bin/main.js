6c86c2dbac93cf0a1d6d6c68455db0b7994e6112
Riley <waskerdu@gmail.com>
2023-09-27 16:51:33 -0700

    change shader to use texture coord matrix

--- bin/main.js
+++ bin/main.js
@@ -4,16 +4,22 @@ const {Texture} = Graphics;
 let tex;
 let spr,spr2;
 Game.init = ()=>{
+    tex = Texture.fromFile('characters_packed.png');
+    tex.sx = 24*2;
+    // tex.sy = 24;
+    tex.sw = 24;
+    tex.sh = 24;
+    System.println(tex.width, tex.height);
     // System.println(''+Texture);
-    spr = Texture.fromFile('character_0000.png');
-    spr2 = Texture.fromFile('character_0000.png');
-    spr.scaleX = 4;
-    spr.originX = 12;
-    spr.originY = 12;
-    spr.angle = 30;
-    tex = Texture.new(400,200);
-    System.println('tex width:', tex.width);
-    System.println('tex height:', tex.height);
+    // spr = Texture.fromFile('character_0000.png');
+    // spr2 = Texture.fromFile('character_0000.png');
+    // spr.scaleX = 4;
+    // spr.originX = 12;
+    // spr.originY = 12;
+    // spr.angle = 30;
+    // tex = Texture.new(400,200);
+    // System.println('tex width:', tex.width);
+    // System.println('tex height:', tex.height);
     // tex.setTarget();
     // spr.draw(100,100);
     // tex.resetTarget();
@@ -25,6 +31,7 @@ Game.init = ()=>{
 
 Game.update = ()=>{
     if(Input.getKey(256)) Game.quit();
-    spr.draw(100, 100);
-    spr2.draw(100, 100);
+    tex.draw(0,0);
+    // spr.draw(100, 100);
+    // spr2.draw(100, 100);
 }
--- bin/vert.glsl
+++ bin/vert.glsl
@@ -6,10 +6,12 @@ out vec2 TexCoord;
 
 uniform mat4 camera;
 uniform mat4 sprite;
-uniform vec4 dimensions;
+uniform mat4 coord;
+// uniform vec4 dimensions;
 
 void main()
 {
     gl_Position = camera * sprite * vec4(aPos, 1.0);
-    TexCoord = aTexCoord * dimensions.zw + dimensions.xy;
+    // TexCoord = aTexCoord * dimensions.zw + dimensions.xy;
+    TexCoord = (coord * vec4(aTexCoord, 1.0, 1.0)).xy;
 }
